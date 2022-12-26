709c7e87f75cee487e659a9fe4509bd9376f86df
Vardhana <vardhanasharma2001@gmail.com>
2022-12-26 13:36:41 +0530

    start practice

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "enter: ";
+    cin>>a;
+    cout <<a<< endl<<"Hello Vardhana" ;
+    cout <<a<< ends<<"Hello Vardhana" ;
+
+    return 0;
+}
